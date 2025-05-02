4d5714a16c30057d855f8664bd9ce2bd035a4c95
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-02 09:15:36 +0530

    load customer method created inside customer controller js

---
+++ Js/Controller/CustomerController.js
@@ -0,0 +1,22 @@
+import {customer_db} from "../DB/db";
+import CustomerModel  from "../Model/CustomerModel";
+
+function loadCustomers() {
+    $('#customer-tbody').empty();
+    customer_db.map((customer)=>{
+        let id = customer.id;
+        let name = customer.name;
+        let address = customer.address;
+        let phone = customer.phone;
+
+        let  data = `\`<tr>
+                            <td>${id}</td>
+                            <td>${name}</td>
+                            <td>${address}</td>
+                            <td>${phone}</td>
+                        </tr>`
+        $('#customer-tbody').append(data);
+
+    })
+}
+
