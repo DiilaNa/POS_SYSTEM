1e2ccbe3fb87eb9e2cbf866b1b9fb3f5fbc30ab7
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-02 13:58:57 +0530

    Item Controller and Model Created

---
+++ Js/Controller/ItemController.js
@@ -0,0 +1,2 @@
+import {item_db} from "../DB/db.js";
+import ItemModel  from "../Model/ItemModel.js";
---
+++ Js/Model/ItemModel.js
@@ -0,0 +1,8 @@
+export default class ItemModel{
+    constructor(Id,name,quantity,price) {
+        this.ItemId = id;
+        this.ItemName = name;
+        this.ItemQuantity = quantity;
+        this.ItemPrice = price;
+    }
+}
